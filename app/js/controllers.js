66db6baa33c4849b72c38eb5995bc8668c2633ec
anthonybrown <tony@tonybrown.me>
2015-07-24 12:16:24 -0400

    setup text messages

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,17 +7,26 @@ angular.module('myApp.controllers', [])
 
 	}])
 	.controller('WaitlistController', ['$scope','$firebase',  function ($scope, $firebase) {
-		var partiesRef = new Firebase('https://waitlist-tonybrown.firebaseio.com/');
-
+		// connect $scope.parties to live firebase data.
+		var partiesRef = new Firebase('https://waitlist-tonybrown.firebaseio.com/parties');
 		//$scope.testVariable = 'tony';
 		$scope.parties = $firebase(partiesRef);
 
+		// Object to store data from form
 		$scope.newParty = { name: '',	phone: '', size: '' }
 
+		// Function to save a new party to the list.
 		$scope.saveParty = function () {
 			$scope.parties.$add($scope.newParty);
 			$scope.newParty = {name: '', phone: '', size: ''}
 		};
+
+		// function to send text messages to parties
+		$scope.sendTextMsg = function (phoneNumber) {
+			var textMessageRef = new Firebase('https://waitlist-tonybrown.firebaseio.com/textMessages');
+			var textMessages = $firebase(textMessageRef);
+			textMessages.$add({ phoneNumber: phoneNumber });
+		}
 
 	}]);
 
